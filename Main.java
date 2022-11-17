600bca069bdecdd89b27403648b3d4eefd04aa46
Jack <115236356+jtoncelli@users.noreply.github.com>
2022-11-16 19:48:47 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,31 +1,31 @@
 import java.util.*;
 class Main {
   public static int userTurn(int coins){
-		//a helper method which prompts the user for a turn and defaults to 2 coins if invalid input is entered
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Coins left: " + coins + ", remove 1 or 2 coins?: ");
-		String input = scan.nextLine();
-		try{
-			int a = Integer.parseInt(input);
-		}catch(Exception e){
-			System.out.println("Invalid input. You will move 2");
-			input = "2";
-		}
-		return Integer.parseInt(input);
+	//a helper method which prompts the user for a turn and defaults to 2 coins if invalid input is entered
+	Scanner scan = new Scanner(System.in);
+	System.out.println("Coins left: " + coins + ", remove 1 or 2 coins?: ");
+	String input = scan.nextLine();
+	try{
+		int a = Integer.parseInt(input);
+	}catch(Exception e){
+		System.out.println("Invalid input. You will move 2");
+		input = "2";
+	}
+	return Integer.parseInt(input);
 }
 public static void winGame(int coins){
 	//is called when a winning condition is ensured
 	//a winning condition is when it is the user's turn and (coins - 4) % 3 == 0 is true
 	//from then on, the computer will do the opposite of the user until it wins
 	while (coins > 0){
-			int userMove = userTurn(coins);
-			coins -= 3;
-			if(coins > 0){
-				if(userMove == 1) System.out.println("Computer removes 2.");
-				else System.out.println("Computer removes 1.");
-			}
-			else System.out.println("The computer wins.");
+		int userMove = userTurn(coins);
+		coins -= 3;
+		if(coins > 0){
+			if(userMove == 1) System.out.println("Computer removes 2.");
+			else System.out.println("Computer removes 1.");
 		}
+		else System.out.println("The computer wins.");
+	}
 }
  public static void main(String[] args) {
    int coins = 23;
