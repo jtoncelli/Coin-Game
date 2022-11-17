91cee135ee73c244ed5c6723e4acf8719597d22c
Jack <115236356+jtoncelli@users.noreply.github.com>
2022-11-16 19:46:49 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -12,65 +12,65 @@ class Main {
 			input = "2";
 		}
 		return Integer.parseInt(input);
-	}
-	public static void winGame(int coins){
-		//is called when a winning condition is ensured
-		//a winning condition is when it is the user's turn and (coins - 4) % 3 == 0 is true
-		//from then on, the computer will do the opposite of the user until it wins
-		while (coins > 0){
-				int userMove = userTurn(coins);
-				coins -= 3;
-				if(coins > 0){
-					if(userMove == 1) System.out.println("Computer removes 2.");
-					else System.out.println("Computer removes 1.");
-				}
-				else System.out.println("The computer wins.");
+}
+public static void winGame(int coins){
+	//is called when a winning condition is ensured
+	//a winning condition is when it is the user's turn and (coins - 4) % 3 == 0 is true
+	//from then on, the computer will do the opposite of the user until it wins
+	while (coins > 0){
+			int userMove = userTurn(coins);
+			coins -= 3;
+			if(coins > 0){
+				if(userMove == 1) System.out.println("Computer removes 2.");
+				else System.out.println("Computer removes 1.");
 			}
-	}
-  public static void main(String[] args) {
-    int coins = 23;
-    Scanner myScan = new Scanner(System.in);
-    System.out.println("\nThe goal is to not pick up the last coin. There are 23 coins initially. \nEnter 1 for user start, enter 2 for computer start: ");
-    String startTurn = myScan.nextLine();
-		startTurn = startTurn.trim();
-		if(startTurn.equals("2")){
-			System.out.println("Computer removes 1.");
-			coins -= 1;
-			//removing one coin from 23 ensures a winning condition, and the game is essentially over
-			winGame(coins);
+			else System.out.println("The computer wins.");
 		}
-		else if(startTurn.equals("1")){
-			while(coins > 0){
-				int userMove = userTurn(coins);
-				coins -= userMove;
-				if(coins <= 0) System.out.println("The computer wins.");
-				else if(coins == 1){
-					coins -= 1;
-					//the ONLY condition in which the user wins; the computer is left with 1 coin
-					System.out.println("You win.");
-				}
-				else if((coins - 5) % 3 == 0 || coins == 2){
-					//the computer will remove two if it satisfies the winning conditions
-					coins -= 1;
-					System.out.println("Computer removes 1.");
-					winGame(coins);
-					break;
-				}
-			  else if((coins - 6) % 3 == 0 || coins == 3){
-					//the computer will remove two if it satisfies the winning conditions
-					coins -=2;
-					System.out.println("Computer removes 2");
-					winGame(coins);
-					break;
-				}
-				else{
-					coins -= 2;
-					System.out.println("Computer removes 2.");
-				}
+}
+ public static void main(String[] args) {
+   int coins = 23;
+   Scanner myScan = new Scanner(System.in);
+   System.out.println("\nThe goal is to not pick up the last coin. There are 23 coins initially. \nEnter 1 for user start, enter 2 for computer start: ");
+   String startTurn = myScan.nextLine();
+	startTurn = startTurn.trim();
+	if(startTurn.equals("2")){
+		System.out.println("Computer removes 1.");
+		coins -= 1;
+		//removing one coin from 23 ensures a winning condition, and the game is essentially over
+		winGame(coins);
+	}
+	else if(startTurn.equals("1")){
+		while(coins > 0){
+			int userMove = userTurn(coins);
+			coins -= userMove;
+			if(coins <= 0) System.out.println("The computer wins.");
+			else if(coins == 1){
+				coins -= 1;
+				//the ONLY condition in which the user wins; the computer is left with 1 coin
+				System.out.println("You win.");
+			}
+			else if((coins - 5) % 3 == 0 || coins == 2){
+				//the computer will remove two if it satisfies the winning conditions
+				coins -= 1;
+				System.out.println("Computer removes 1.");
+				winGame(coins);
+				break;
+			}
+		  else if((coins - 6) % 3 == 0 || coins == 3){
+				//the computer will remove two if it satisfies the winning conditions
+				coins -=2;
+				System.out.println("Computer removes 2");
+				winGame(coins);
+				break;
+			}
+			else{
+				coins -= 2;
+				System.out.println("Computer removes 2.");
 			}
 		}
-		else{
-			System.out.println("Hope you play next time!");
-		}
-  }
+	}
+	else{
+		System.out.println("Hope you play next time!");
+	}
+ }
 }
